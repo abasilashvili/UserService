87b1975527258124ac0248429e6be839dd47a3f2
abasilashvili <alexcodebro@gmail.com>
2025-03-27 20:29:17 +0300

    Added Recommendation CRUD.

---
+++ src/main/java/com/abasilashvili/user_service/dto/recommendation/SkillOfferDto.java
@@ -0,0 +1,9 @@
+package com.abasilashvili.user_service.dto.recommendation;
+
+import lombok.Data;
+
+@Data
+public class SkillOfferDto {
+    private Long id;
+    private Long skillId;
+}
---
+++ src/main/java/com/abasilashvili/user_service/mappers/RecommendationDtoMapper.java
@@ -0,0 +1,12 @@
+package com.abasilashvili.user_service.mappers;
+
+import com.abasilashvili.user_service.dto.recommendation.RecommendationDto;
+import com.abasilashvili.user_service.entity.recommendation.Recommendation;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface RecommendationDtoMapper {
+    RecommendationDto toDto(Recommendation recommendation);
+    Recommendation toEntity(RecommendationDto recommendationDto);
+}
